bf9b6baf7e5c8742e2a2c5c90dcb0111211125ac
Cooper <64923856+csjamieson@users.noreply.github.com>
2021-08-20 14:00:44 -0700

    Add files via upload

---
+++ checkBridge.ts
@@ -0,0 +1,68 @@
+import 'dotenv/config';
+import Discord, { TextChannel } from 'discord.js';
+import fetch from 'node-fetch';
+import { ethers } from "ethers";
+
+const OPENSEA_SHARED_STOREFRONT_ADDRESS = '0x495f947276749Ce646f68AC8c248420045cb7b5e';
+
+const discordBot = new Discord.Client();
+const  discordSetup = async (): Promise<TextChannel> => {
+  return new Promise<TextChannel>((resolve, reject) => {
+    ['DISCORD_BOT_TOKEN', 'DISCORD_CHANNEL_ID'].forEach((envVar) => {
+      if (!process.env[envVar]) reject(`${envVar} not set`)
+    })
+  
+    discordBot.login(process.env.DISCORD_BOT_TOKEN);
+    discordBot.on('ready', async () => {
+      const channel = await discordBot.channels.fetch(process.env.DISCORD_CHANNEL_ID!);
+      resolve(channel as TextChannel);
+    });
+  })
+}
+
+const buildMessage = (sale: any) => (
+  new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle(sale.asset.name)
+	.setURL(sale.asset.permalink)
+	.addFields(
+		{ name: 'Token Bridged'},
+	)
+  .setImage(sale.asset.image_url)
+	.setTimestamp(Date.parse(`${sale?.created_date}Z`))
+)
+
+async function main() {
+  const channel = await discordSetup();
+  const seconds = process.env.SECONDS ? parseInt(process.env.SECONDS) : 3_600;
+  const hoursAgo = (Math.round(new Date().getTime() / 1000) - (seconds)); // in the last hour, run hourly?
+  
+  const params = new URLSearchParams({
+    offset: '0',
+    limit: '1',
+    event_type: 'transfer',
+    only_opensea: 'false',
+    account_address: '0xbcb7d6dc7049751ac77878366bf02550930cb464'
+    occurred_after: hoursAgo.toString(), 
+  })
+
+  const openSeaResponse = await fetch(
+    "https://api.opensea.io/api/v1/events?" + params).then((resp) => resp.json());
+    
+  return await Promise.all(
+    openSeaResponse?.asset_events?.reverse().map(async (sale: any) => {
+      const message = buildMessage(sale);
+      return channel.send(message)
+    })
+  );   
+}
+
+main()
+  .then((res) =>{ 
+    if (!res.length) console.log("No recent sales")
+    process.exit(0)
+  })
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
